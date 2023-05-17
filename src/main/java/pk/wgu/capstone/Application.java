80608792023b84cdf94b3c46c589e7da7d99d4a9
gitpk-0 <kell108p@protonmail.com>
2023-05-17 10:34:36 -0400

    icon, offline image and html

--- src/main/java/pk/wgu/capstone/Application.java
+++ src/main/java/pk/wgu/capstone/Application.java
@@ -1,19 +1,24 @@
 package pk.wgu.capstone;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * The entry point of the Spring Boot application.
- *
- * Use the @PWA annotation make the application installable on phones, tablets
- * and some desktop browsers.
- *
  */
 @SpringBootApplication
 @Theme(value = "prospero")
+@PWA(
+        name = "Prospero",
+        shortName = "Prospero",
+        offlinePath = "offline.html",
+        offlineResources = {"images/offline.png"}
+)
+@NpmPackage(value = "line-awesome", version = "1.3.0")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
