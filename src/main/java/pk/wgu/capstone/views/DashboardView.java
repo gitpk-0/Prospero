9d7d8950754c9b3775e199c6bd5fc9c65a221651
gitpk-0 <kell108p@protonmail.com>
2023-06-10 17:01:01 -0400

    dashboard structure in place

--- src/main/java/pk/wgu/capstone/views/DashboardView.java
+++ src/main/java/pk/wgu/capstone/views/DashboardView.java
@@ -2,12 +2,18 @@ package pk.wgu.capstone.views;
 
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.board.Board;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.select.Select;
 import com.vaadin.flow.data.binder.ValueContext;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -24,6 +30,8 @@ import java.time.LocalDate;
 @Route(value = "", layout = MainLayout.class)
 @PageTitle("Dashboard | Prospero")
 @PermitAll
+@CssImport(value = "./themes/prospero/prospero-charts.css", themeFor = "vaadin-chart")
+@CssImport(value = "./themes/prospero/views/dashboard-view.css")
 public class DashboardView extends Main {
 
     private final SecurityService securityService;
@@ -42,13 +50,14 @@ public class DashboardView extends Main {
         String firstName = service.findUserById(userId).getFirstName();
         String currentMonth = LocalDate.now().getMonth().toString();
 
-
         Board board = new Board();
         board.addRow(
                 createWelcomeHighlight("Welcome " + firstName, userId),
                 createHighlight("Income", getIncomeTotal(userId), currentMonth),
                 createHighlight("Expenses", getExpensesTotal(userId), currentMonth),
                 createHighlight("Transactions", getTransactionCount(userId), currentMonth));
+        board.addRow(createYearViewChart());
+        board.addRow(createIncomePieChart(), createExpensePieChart());
         add(board);
     }
 
@@ -124,6 +133,123 @@ public class DashboardView extends Main {
         layout.addClassName(LumoUtility.Padding.LARGE);
         layout.setPadding(false);
         layout.setSpacing(false);
+        return layout;
+    }
+
+    private Component createYearViewChart() {
+        Select year = new Select();
+        year.setItems("2020", "2021", "2022", "2023");
+        year.setValue("2022");
+        year.setWidth("100px");
+
+        HorizontalLayout header = createHeader("Year View", "Transaction Totals by Month");
+        header.add(year);
+
+        Chart yearViewChart = new Chart(ChartType.AREASPLINE);
+        yearViewChart.addClassName("year-view-chart");
+        Configuration config = yearViewChart.getConfiguration();
+        config.getChart().setStyledMode(true);
+        // yearViewChart.setThemeName("classic");
+
+        XAxis xAxis = new XAxis();
+        xAxis.setCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+        config.addxAxis(xAxis);
+
+        config.getyAxis().setTitle("Totals");
+
+        PlotOptionsAreaspline plotOptionsIncome = new PlotOptionsAreaspline();
+        plotOptionsIncome.setColorIndex(4);  //#90ed7d
+        plotOptionsIncome.setPointPlacement(PointPlacement.ON);
+        plotOptionsIncome.setMarker(new Marker(false));
+
+
+        ListSeries income = new ListSeries("Income", 189, 191, 291, 396, 501, 403, 609, 712, 729, 942, 1044, 1247);
+        income.setPlotOptions(plotOptionsIncome);
+        config.addSeries(income);
+
+        PlotOptionsAreaspline plotOptionsExpense = new PlotOptionsAreaspline();
+        plotOptionsExpense.setColorIndex(2);
+        plotOptionsExpense.setPointPlacement(PointPlacement.ON);
+        plotOptionsExpense.setMarker(new Marker(false));
+
+        ListSeries expense = new ListSeries("Expense", 138, 246, 248, 348, 352, 353, 463, 573, 778, 779, 885, 887);
+        expense.setPlotOptions(plotOptionsExpense);
+        config.addSeries(expense);
+
+
+        VerticalLayout layout = new VerticalLayout(header, yearViewChart);
+        layout.addClassName(LumoUtility.Padding.LARGE);
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        layout.getElement().getThemeList().add("spacing-l");
+        return layout;
+    }
+
+    private HorizontalLayout createHeader(String title, String subtitle) {
+        H2 h2 = new H2(title);
+        h2.addClassNames(LumoUtility.FontSize.XLARGE, LumoUtility.Margin.NONE);
+
+        Span span = new Span(subtitle);
+        span.addClassNames(LumoUtility.TextColor.SECONDARY, LumoUtility.FontSize.XSMALL);
+
+        VerticalLayout column = new VerticalLayout(h2, span);
+        column.setPadding(false);
+        column.setSpacing(false);
+
+        HorizontalLayout header = new HorizontalLayout(column);
+        header.setJustifyContentMode(FlexComponent.JustifyContentMode.BETWEEN);
+        header.setSpacing(false);
+        header.setWidthFull();
+        return header;
+    }
+
+    private Component createIncomePieChart() {
+        HorizontalLayout header = createHeader("Income", "All Time");
+
+        Chart incomePieChart = new Chart(ChartType.PIE);
+        Configuration config = incomePieChart.getConfiguration();
+        config.getChart().setStyledMode(true);
+        incomePieChart.setThemeName("gradient");
+
+        DataSeries incomeSeries = new DataSeries();
+        incomeSeries.add(new DataSeriesItem("Salary" ,12.5));
+        incomeSeries.add(new DataSeriesItem("Bonus" ,12.5));
+        incomeSeries.add(new DataSeriesItem("Commission" ,12.5));
+        incomeSeries.add(new DataSeriesItem("Gift" ,12.5));
+        incomeSeries.add(new DataSeriesItem("Other" ,12.5));
+        incomeSeries.add(new DataSeriesItem("Investment" ,12.5));
+        config.addSeries(incomeSeries);
+
+        VerticalLayout layout = new VerticalLayout(header, incomePieChart);
+        layout.addClassName(LumoUtility.Padding.LARGE);
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        layout.getElement().getThemeList().add("spacing-l");
+        return layout;
+    }
+
+    private Component createExpensePieChart() {
+        HorizontalLayout header = createHeader("Expense", "All Time");
+
+        Chart expensePieChart = new Chart(ChartType.PIE);
+        Configuration config = expensePieChart.getConfiguration();
+        config.getChart().setStyledMode(true);
+        expensePieChart.setThemeName("classic");
+
+        DataSeries expenseSeries = new DataSeries();
+        expenseSeries.add(new DataSeriesItem("Salary" ,12.5));
+        expenseSeries.add(new DataSeriesItem("Bonus" ,12.5));
+        expenseSeries.add(new DataSeriesItem("Commission" ,12.5));
+        expenseSeries.add(new DataSeriesItem("Gift" ,12.5));
+        expenseSeries.add(new DataSeriesItem("Other" ,12.5));
+        expenseSeries.add(new DataSeriesItem("Investment" ,12.5));
+        config.addSeries(expenseSeries);
+
+        VerticalLayout layout = new VerticalLayout(header, expensePieChart);
+        layout.addClassName(LumoUtility.Padding.LARGE);
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        layout.getElement().getThemeList().add("spacing-l");
         return layout;
     }
 }
