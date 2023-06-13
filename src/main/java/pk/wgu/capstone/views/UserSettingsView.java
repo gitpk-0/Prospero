d692d0c23d7509206e69ce04437c580aa782f89b
gitpk-0 <kell108p@protonmail.com>
2023-06-13 13:36:35 -0400

    user settings - change app theme

--- src/main/java/pk/wgu/capstone/views/UserSettingsView.java
+++ src/main/java/pk/wgu/capstone/views/UserSettingsView.java
@@ -10,6 +10,7 @@ import com.vaadin.flow.component.details.DetailsVariant;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -18,6 +19,7 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import jakarta.annotation.security.PermitAll;
 import pk.wgu.capstone.data.entity.User;
@@ -43,6 +45,8 @@ public class UserSettingsView extends Div {
 
     private Accordion accordion;
 
+    boolean isDarkTheme = true;
+
     private Binder<User> userBinder = new Binder<>(User.class);
 
     private Long userId;
@@ -63,6 +67,8 @@ public class UserSettingsView extends Div {
         add(createTitle());
         add(createMainAccordion());
         add(createButtonLayout());
+
+        accordion.close();
 
         userBinder.bindInstanceFields(this);
 
@@ -94,19 +100,40 @@ public class UserSettingsView extends Div {
 
     private Component createMainAccordion() {
         accordion = new Accordion();
-
+        accordion.addClassName("main-accordion");
 
         AccordionPanel userInfo = accordion.add("User Information", createUserInfoFormLayout());
         userInfo.addThemeVariants(DetailsVariant.REVERSE, DetailsVariant.FILLED);
+        userInfo.addClassName(LumoUtility.Margin.Vertical.MEDIUM);
 
-        AccordionPanel appTheme = accordion.add("App Theme", new VerticalLayout(new H3("change app theme")));
+        AccordionPanel appTheme = accordion.add("App Theme", createChangeAppThemeButton());
         appTheme.addThemeVariants(DetailsVariant.REVERSE, DetailsVariant.FILLED);
+        appTheme.addClassName(LumoUtility.Margin.Vertical.MEDIUM);
 
         AccordionPanel advSettings = accordion.add(
                 "Advanced Settings", new VerticalLayout(createAdvSettingsAccordion()));
         advSettings.addThemeVariants(DetailsVariant.REVERSE, DetailsVariant.FILLED);
+        appTheme.addClassName(LumoUtility.Margin.Vertical.MEDIUM);
 
         return accordion;
+    }
+
+    private Component createChangeAppThemeButton() {
+        // light/dark mode toggle button
+        Button changeThemeBtn = new Button("Change App Theme", VaadinIcon.MOON.create());
+        // changeThemeBtn.addClassName("theme-btn");
+        changeThemeBtn.getElement().setAttribute("title", "Change current theme");
+        changeThemeBtn.addClickListener(e -> {
+            if (isDarkTheme) {
+                setTheme(false);
+                isDarkTheme = false;
+            } else {
+                setTheme(true);
+                isDarkTheme = true;
+            }
+        });
+
+        return changeThemeBtn;
     }
 
     private Component createAdvSettingsAccordion() {
@@ -121,9 +148,10 @@ public class UserSettingsView extends Div {
 
     private Component createUserInfoFormLayout() {
         FormLayout userInfoForm = new FormLayout();
+        firstName.setErrorMessage("First name cannot be blank");
+        lastName.setErrorMessage("Last name cannot be blank");
         email.setErrorMessage("Please enter a valid email address");
         userInfoForm.add(firstName, lastName, email, allowMarketing, createButtonLayout());
-        // userInfoForm.add(createButtonLayout());
         return userInfoForm;
     }
 
@@ -133,6 +161,12 @@ public class UserSettingsView extends Div {
         updateBtn.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         buttonLayout.add(updateBtn);
         buttonLayout.add(cancelBtn);
+        buttonLayout.addClassNames(LumoUtility.Margin.Vertical.AUTO);
         return buttonLayout;
+    }
+
+    private void setTheme(boolean dark) {
+        var js = "document.documentElement.setAttribute('theme', $0)";
+        getElement().executeJs(js, dark ? Lumo.DARK : Lumo.LIGHT);
     }
 }
